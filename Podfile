7a8d5390b7918d75a1af7f2b8b1e873c787d4b4a
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2016-01-23 14:35:26 -0500

    Charts

--- Podfile
+++ Podfile
@@ -4,3 +4,4 @@ use_frameworks!
 
 pod 'Alamofire', '~> 3.0'
 pod 'SnapKit', '~> 0.15.0'
+pod 'Charts'
--- cai/ViewController.swift
+++ cai/ViewController.swift
@@ -10,54 +10,75 @@ import AVFoundation
 import UIKit
 import Alamofire
 import SnapKit
+import Charts
 
 class ViewController: UIViewController {
     var audioRecorder: AVAudioRecorder!
     var filePath: NSURL?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let english = UILabel()
+        english.text = "wǒ"
+        english.font = UIFont(name: english.font.fontName, size: 40)
+        let chinese = UILabel()
+        chinese.text = "我"
+        chinese.font = UIFont(name: chinese.font.fontName, size: 40)
+
+        self.view.addSubview(english)
+        self.view.addSubview(chinese)
+        english.snp_makeConstraints { (make) -> Void in
+            make.left.equalTo(self.view).offset(100)
+            make.top.equalTo(self.view).offset(130)
+        }
+        chinese.snp_makeConstraints { (make) -> Void in
+            make.right.equalTo(self.view).offset(-100)
+            make.top.equalTo(self.view).offset(130)
+        }
+
+        
         let recordButton = UIButton(type: UIButtonType.System) as UIButton
-        recordButton.frame = CGRectMake(100, 100, 100, 50)
-        recordButton.setTitle("we out here", forState: UIControlState.Normal)
+        recordButton.titleLabel!.font = UIFont(name: "HelveticaNeue-Thin", size: 25)
+        recordButton.setTitle("record", forState: UIControlState.Normal)
         recordButton.addTarget(self, action: Selector("startRecording:"), forControlEvents: UIControlEvents.TouchDown)
         recordButton.addTarget(self, action: Selector("endRecording:"), forControlEvents: UIControlEvents.TouchUpInside)
         self.view.addSubview(recordButton)
         recordButton.snp_makeConstraints { (make) -> Void in
-            make.top.equalTo(self.view).offset(20)
-            make.left.equalTo(self.view).offset(20)
-            make.bottom.equalTo(self.view).offset(-20)
-            make.right.equalTo(self.view).offset(-20)
+            make.left.equalTo(self.view).offset(80)
+            make.bottom.equalTo(self.view).offset(-30)
+            make.right.equalTo(self.view).offset(-80)
         }
     }
 
     func startRecording(sender: UIButton!) {
         let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
-        
+
         let recordingName = "sound.wav"
         let pathArray = [dirPath, recordingName]
         filePath = NSURL.fileURLWithPathComponents(pathArray)
-        
+
         let session = AVAudioSession.sharedInstance()
         try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
-        
+
         try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
         audioRecorder.meteringEnabled = true
         audioRecorder.prepareToRecord()
         audioRecorder.record()
     }
-    
+
     func endRecording(sender: UIButton!) {
         audioRecorder.stop()
         showGraph()
     }
-    
+
     func showGraph() {
         if let data = NSData(contentsOfURL: filePath!) {
-            Alamofire.upload(.POST, "meow.com/", data: data)
-            .responseJSON { _ in }
+            Alamofire
+                .upload(.POST, "meow.com/", data: data)
+                .responseJSON { _ in }
+
         }
     }
-    
+
 }
