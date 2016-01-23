7357a4a7a958372452d5d622f08e4975e4c0f8ed
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2016-01-23 02:48:10 -0500

    Add recording

--- cai/ViewController.swift
+++ cai/ViewController.swift
@@ -6,12 +6,16 @@
 //  Copyright © 2016 Vignesh Mohankumar. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let recordButton = UIButton(type: UIButtonType.System) as UIButton
         recordButton.frame = CGRectMake(100, 100, 100, 50)
         recordButton.setTitle("we out here", forState: UIControlState.Normal)
@@ -21,11 +25,28 @@ class ViewController: UIViewController {
     }
 
     func startRecording(sender: UIButton!) {
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        
+        let recordingName = "sound.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        
+        try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
         print("start")
     }
     
     func endRecording(sender: UIButton!) {
+        audioRecorder.stop()
         print("end")
     }
+    
+    func sendSound(String
 
 }
